0c391ee771a8eb976a0cfb150d6d3f59e12ca9cd
ayush rusia <70137761+AyushRusia@users.noreply.github.com>
2020-09-24 16:17:03 +0530

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -1,7 +1,7 @@
 function timer()
 {
     // Set the date we're counting down to
-var countDownDate = new Date("Sep 24, 2020 00:00:00").getTime();
+var countDownDate = new Date("Oct 6, 2020 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
