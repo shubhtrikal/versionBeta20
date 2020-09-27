22bbb8b9b4054f82626bf4b7c3e5678662c4f33f
ayush rusia <70137761+AyushRusia@users.noreply.github.com>
2020-09-27 20:53:32 +0530

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -1,5 +1,5 @@
-function timer()
-{
+
+
     // Set the date we're counting down to
 var countDownDate = new Date("Oct 13, 2020 00:00:00").getTime();
 
@@ -30,7 +30,7 @@ var x = setInterval(function() {
     document.getElementById("timer").innerHTML = "";
   }
 }, 1000);
-}
+
 
 const faqBtn = document.querySelectorAll(".btn")
 
@@ -47,3 +47,127 @@ faqBtn.forEach(function (button) {
     })
 })
 
+
+let content = ["Version β 3.0 ", "IDE for your Code", "Now Online... ", "It's all Talk until Code Runs"];
+
+let part = 0;
+
+let partIndex = 0;
+
+let interval;
+
+let element = document.querySelector("#text-typing");
+
+// Implements typing effect
+function Type() {
+    let text = content[part].substring(0, partIndex + 1);
+    element.innerHTML = text;
+    partIndex++;
+
+    // If full sentence has been displayed then start to delete the sentence after some time
+    if (text === content[part]) {
+        clearInterval(interval);
+        setTimeout(function () {
+            interval = setInterval(Delete, 50);
+        }, 1000);
+    }
+}
+
+// Implements deleting effect
+function Delete() {
+    let text = content[part].substring(0, partIndex - 1);
+    element.innerHTML = text;
+    partIndex--;
+
+    // If sentence has been deleted then start to display the next sentence
+    if (text === '') {
+        clearInterval(interval);
+
+        // If last sentence then display the first one, else move to the next
+        if (part == (content.length - 1))
+            part = 0;
+        else
+            part++;
+        partIndex = 0;
+
+        // Start to display the next sentence after some time
+        setTimeout(function () {
+            interval = setInterval(Type, 100);
+        }, 200);
+    }
+}
+
+// Start the typing effect on load
+interval = setInterval(Type, 100);
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+//scroll animations//
+
+     //Get the button
+     var mybutton = document.getElementById("myBtn");
+          
+     window.onscroll = function() {scrollFunction()};
+     
+     function scrollFunction() {
+       if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+         mybutton.style.display = "block";
+       } else {
+         mybutton.style.display = "none";
+       }
+       /////////////////////////////////////////////////////////////////////////////////////////
+       var navi =document.getElementsByClassName("nav-link");
+
+       if (document.documentElement.scrollTop > 0 && document.documentElement.scrollTop <=400 )
+       {
+         for(var i=0;i<navi.length;i++)
+         {
+           if(navi[i].classList.contains("active"))
+             navi[i].classList.remove("active");
+         }
+         navi[0].classList.add("active");
+        }
+         
+if (document.documentElement.scrollTop > 400 && document.documentElement.scrollTop <=1200 )
+{
+  for(var i=0;i<navi.length;i++)
+  {
+    if(navi[i].classList.contains("active"))
+      navi[i].classList.remove("active");
+  }
+  navi[1].classList.add("active");
+}
+if (document.documentElement.scrollTop > 1200 && document.documentElement.scrollTop <=2000 )
+{
+  for(var i=0;i<navi.length;i++)
+  {
+    if(navi[i].classList.contains("active"))
+      navi[i].classList.remove("active");
+  }
+  navi[2].classList.add("active");
+}
+if (document.documentElement.scrollTop > 2000 && document.documentElement.scrollTop <=3500 )
+{
+  for(var i=0;i<navi.length;i++)
+  {
+    if(navi[i].classList.contains("active"))
+      navi[i].classList.remove("active");
+  }
+  navi[4].classList.add("active");
+}
+if (document.documentElement.scrollTop > 3500 && document.documentElement.scrollTop <=3900 )
+{
+  for(var i=0;i<navi.length;i++)
+  {
+    if(navi[i].classList.contains("active"))
+      navi[i].classList.remove("active");
+  }
+  navi[6].classList.add("active");
+}
+
+      }
+     
+     // When the user clicks on the button, scroll to the top of the document
+     function topFunction() {
+       document.body.scrollTop = 0;
+       document.documentElement.scrollTop = 0;
+     }
